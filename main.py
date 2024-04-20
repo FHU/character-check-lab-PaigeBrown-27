65a327190f870eedc6d051f0d9b3233d53a89987
PaigeBrown-27 <paige.brown@students.fhu.edu>
2024-04-20 14:21:19 -0500

    character == done?

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
+   for i in word:
+      character = word[index] 
+      if character in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+         return "digit"
+      elif character == " ":
+         return "white space" 
+      elif character in ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]:
+         return "letter" 
+      else:
+         return "unknown"
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
